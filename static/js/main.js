1b618503aa3e1b453a21506edff1d82881599f19
shenc <stephenshenc@gmail.com>
2025-05-30 13:02:41 +0800

    init

---
+++ static/js/main.js
@@ -0,0 +1,20 @@
+const originalTitleElement = document.querySelector('.dynamic-title');
+const title = originalTitleElement.innerHTML;
+originalTitleElement.innerHTML = ' ';
+
+let titleIndex = 0;
+function typeTitle() {
+    if (titleIndex < title.length) {
+        originalTitleElement.innerHTML += title.charAt(titleIndex);
+        titleIndex++;
+        setTimeout(typeTitle, 400);
+    }
+    else {
+        originalTitleElement.innerHTML = ' ';
+        titleIndex = 0;
+        setTimeout(typeTitle, 400);
+    }
+}
+
+
+typeTitle();
